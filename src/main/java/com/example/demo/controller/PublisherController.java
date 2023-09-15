879646734399f7ada720dbaa31f3b340a4708f93
Wilgen Garcia <wilgengarcia2001@gmail.com>
2023-09-14 22:05:47 -0500

    add publisher controller

---
+++ src/main/java/com/example/demo/controller/PublisherController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.repository.PublisherRepository;
+
+@Controller
+public class PublisherController {
+    private final PublisherRepository publisherRepository;
+
+    public PublisherController(PublisherRepository publisherRepository) {
+        this.publisherRepository = publisherRepository;
+    }
+    
+    @RequestMapping("/publishers")
+    public String getPublishers(Model model){
+        publisherRepository.findAll();
+        return "publishers/list";
+    }
+}
